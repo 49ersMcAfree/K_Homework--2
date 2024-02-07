9a32b792cdb232e9e05227eb1c233058471f9fea
CWill22 <147526756+CWill22@users.noreply.github.com>
2024-02-07 15:40:13 -0600

    Fixed reading from file, put Student class inside Studentmanager file

--- K_Homework2/student/edu/studentmanager.java
+++ K_Homework2/student/edu/studentmanager.java
@@ -3,6 +3,7 @@
  */
 package student.edu;
 
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -11,21 +12,22 @@ import java.util.Scanner;
 /**
  * 
  */
-public class studentmanager {
+public class StudentManager {
 
+	
+	private Student[] students = new Student[10];
 	/**
 	 * 
 	 */
-	public studentmanager() {
+	public StudentManager() {
 		// TODO Auto-generated constructor stub
-	
 
 	}
 	
 	public boolean readFromFile(String fileName) {
 		try {
 			Scanner fileIn = new Scanner(new FileInputStream("studentData.txt"));
-			Student[] students = new Student;
+			//Student[] students = new Student[5];
 			int i=0;
 			// Read the data separated by whitespaces and new lines
 //			while(fileIn.hasNext()) {
@@ -33,40 +35,122 @@ public class studentmanager {
 //			}
 			
 			// Read the Data line by line
-			while(fileIn.hasNextLine()) {
-
-//				System.out.println("Line: " + fileIn.nextLine());
-				S_id = fileIn.next();
-				S_name = fileIn.next();
-				S_grade = fileIn.next();
-				Student student[i] = new Student(S_id, S_name, S_grade);
+			while(fileIn.hasNext()) {
+				
+				//System.out.println("Line: " + fileIn.nextLine());
+				int S_id = fileIn.nextInt();
+				System.out.println(S_id);
+				String S_name = fileIn.next();
+				System.out.println(S_name+"HERE!!!!2");
+				String S_name2 = fileIn.next();
+				System.out.println(S_name2+"HERE!!!!2");
+				double S_grade = fileIn.nextDouble();
+				System.out.println(S_grade+"HERE!!!!3");
+				System.out.println("");
+				students[i] = new Student(S_id, S_name, S_grade);
+				System.out.println(S_id+"\t"+S_name+"\t"+S_name2+"\t"+S_grade);
 				i++;
 				//System.out.println("Name: " + name + ", Age: " + age);	
 			}
 			return true;
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 		return false;
 	}
 	
 	public void displayStudents() {
 		// Display the students
-		if (student == NULL) {
+		if (students.length == 0) {
 			System.out.println("No students to display");
 		} else {
 			for (int i = 0; i < students.length; i++) {
-				toString(students[i]);
+				students[i].toString();
 			}
 		}
 	}
 
+	public boolean searchStudentById(int id) {
+		// Search for a student by ID
+		for (int i = 0; i < students.length; i++) {
+			if ((students[i].equals(id))) {
+				System.out.println("Student found: " + students[i].toString());
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public class Student {
+	    private int id;
+	    private String name;
+	    private double grade;
+		
+
+		/**
+		 * 
+		 */
+		public Student() {
+			// TODO Auto-generated constructor stub
+		}
+		// Parameterized constructor
+	    public Student(int s_id, String name, double s_grade) {
+	        this.id = s_id;
+	        this.name = name;
+	        this.grade = s_grade;
+	    }
+
+	    // Getter and Setter 
+	    public int getId() {
+	        return id;
+	    }
+
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	    public double getGrade() {
+	        return grade;
+	    }
+	    public void setGrade(double grade) {
+	        this.grade = grade;
+	    }
+
+	    // toString method to display student information
+	    public String toString() {
+	    	
+	    
+	         return "Student [id=" + id + ", name=" + name + ", grade=" + grade + "]";
+
+	    }
+	}
+/*
+	public boolean updateStudentGradeById(int id, double grade) {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
+	
 
 }
+
+
+
+//The bottom down is commented out for now
+    /*// Main method for demonstration
+
+	/**
+	 * @param args
+	 */
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//} 
+
+//}
