55bf37441fe14e3eb994b6d0b684c693e3806c85
Alasdair Orr <158096722+49ersMcAfree@users.noreply.github.com>
2024-02-07 20:33:08 -0600

    Update studentmanager.java

--- K_Homework2/student/edu/studentmanager.java
+++ K_Homework2/student/edu/studentmanager.java
@@ -88,48 +88,57 @@ public class StudentManager {
 		
 
 		/**
-		 * 
-		 */
-		public Student() {
-			// TODO Auto-generated constructor stub
-		}
-		// Parameterized constructor
-	    public Student(int s_id, String name, double s_grade) {
-	        this.id = s_id;
-	        this.name = name;
-	        this.grade = s_grade;
-	    }
+ * 
+ */
 
-	    // Getter and Setter 
-	    public int getId() {
-	        return id;
-	    }
+/**
+ * 
+ */
+public class Student {
+    private int id;
+    private String name;
+    private double grade;
 
-	    public void setId(int id) {
-	        this.id = id;
-	    }
-	    public String getName() {
-	        return name;
-	    }
-
-	    public void setName(String name) {
-	        this.name = name;
-	    }
-	    public double getGrade() {
-	        return grade;
-	    }
-	    public void setGrade(double grade) {
-	        this.grade = grade;
-	    }
-
-	    // toString method to display student information
-	    public String toString() {
-	    	
-	    
-	         return "Student [id=" + id + ", name=" + name + ", grade=" + grade + "]";
-
-	    }
+	/**
+	 * 
+	 */
+ public Student() {
+		// TODO Auto-generated constructor stub
 	}
+// Parameterized constructor
+public Student(int id, String name, double grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    // Getter and Setter 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getGrade() {
+        return grade;
+    }
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    // toString method to display student information
+    @Override
+    public String toString() {
+         return "Student [id=" + id + ", name=" + name + ", grade=" + grade + "]";
+
+    }
 /*
 	public boolean updateStudentGradeById(int id, double grade) {
 	/**
